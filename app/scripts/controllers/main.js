ae5a0641337c58dd5d1a6f9e7d8f173c6c9dc55b
Danny Palmer <dannyp@bnj.com>
2014-02-14 11:53:38 -0800

    angular init from yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('cassie30App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
