1d276501c33d76db259ecbefd726d2fde742b2ed
Danny Palmer <dannyp@bnj.com>
2014-02-14 12:20:10 -0800

    setup for hastags

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,5 @@
 
 angular.module('cassie30App')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.hashtag = 'tag';
   });
