c36d88513cdee67680ec63d0e001eaae1295c1a2
Danny Palmer <dannyp@bnj.com>
2014-02-17 11:56:21 -0800

    basic setup for instagram feed (showing popular)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('cassie30App', [
+var app = angular.module('cassie30App', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
@@ -16,3 +16,20 @@ angular.module('cassie30App', [
         redirectTo: '/'
       });
   });
+
+app.factory('instagram', ['$http', function($http){
+
+  return {
+    fetchPopular: function(callback){
+
+      var endPoint = 'https://api.instagram.com/v1/media/popular?client_id=642176ece1e7445e99244cec26f4de1f&callback=JSON_CALLBACK';
+
+      $http.jsonp(endPoint).success(function(response){
+        callback(response.data);
+      });
+
+    }
+
+  };
+
+}]);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,22 @@
 'use strict';
 
-angular.module('cassie30App')
-  .controller('MainCtrl', function ($scope) {
-    $scope.hashtag = 'tag';
+//angular.module('cassie30App')
+//  .controller('MainCtrl', function ($scope) {
+//    $scope.hashtag = 'tag';
+//  });
+
+
+angular.module('cassie30App').controller('MainCtrl', ['$scope', 'instagram' ,
+function ($scope, instagram){
+
+  $scope.hashtag = 'tag';
+
+  $scope.layout = 'grid';
+
+  $scope.pics = [];
+
+  instagram.fetchPopular(function(data){
+    $scope.pics = data;
   });
+
+}]);
