bde5944d53afff180541d2999b3098033e38fd26
Danny Palmer <danny@dannyprose.com>
2014-03-02 23:48:50 -0800

    add in normalize

--- config.rb
+++ config.rb
@@ -53,6 +53,11 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+# Bower
+ready do
+  sprockets.append_path File.join root, 'bower_components'
+end
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
