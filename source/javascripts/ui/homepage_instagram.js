555bcebd3f123cba261a63dde3ec2a79a1cbc41a
Danny Palmer <danny@dannyprose.com>
2014-03-13 22:45:50 -0700

    basic setup for jquery-instagram

--- source/javascripts/ui/homepage_instagram.js
+++ source/javascripts/ui/homepage_instagram.js
@@ -4,22 +4,45 @@
 // Pull in all photos tagged with a certain hashtag
 ///////////////////////////////////////////////////
 
-$(document).ready(function() {
+function createPhotoElement(photo) {
+      var innerHtml = $('<img>')
+        .addClass('instagram-image')
+        .attr('src', photo.images.thumbnail.url);
 
-  //Initialize Instafeed.js on load
-  var feed = new Instafeed({
-    get: 'tagged',
-    tagName: 'video',
-    clientId: '20c5389504fc402d983e5685b859d6ba',
-    template: '<a class="animation" href="{{link}}"><img src="{{image}}" /></a><div>{{model.type}}</div>'
-  });
-  feed.run();
+      innerHtml = $('<a>')
+        .attr('target', '_blank')
+        .attr('href', photo.link)
+        .append(innerHtml);
+
+      return $('<div>')
+        .addClass('instagram-placeholder')
+        .attr('id', photo.id)
+        .append(innerHtml);
+    }
+
+function didLoadInstagram(event, response) {
+      var that = this;
+
+      $.each(response.data, function(i, photo) {
+        $(that).append(createPhotoElement(photo));
+      });
+    }
+
+$(document).ready(function() {
+  var clientId = '20c5389504fc402d983e5685b859d6ba';
+
+  $('#instagram').on('didLoadInstagram', didLoadInstagram);
+        $('#instagram').instagram({
+          hash: 'video',
+          count: 50,
+          clientId: clientId
+        });
 
   // Set a refresh of Instafeed every minute
-  var instafeedRefreshTimer = setInterval(RefreshTimer, 60000);
-  function RefreshTimer() {
-    feed.run();
-    scrollTop: $("#instafeed").offset().top
-  }
+  //var instafeedRefreshTimer = setInterval(RefreshTimer, 60000);
+  //function RefreshTimer() {
+  //  feed.run();
+  //  scrollTop: $("#instafeed").offset().top
+  //}
 
 });
