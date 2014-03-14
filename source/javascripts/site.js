98b10e9126b373aa7d8e5c9ad93483ad384a2a9c
Danny Palmer <danny@dannyprose.com>
2014-03-13 23:03:22 -0700

    remove jquery plugins for instagram

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,5 +1,4 @@
 // Vendor - Plugins - jQuery
-//= require instafeed.js/instafeed.js
 
 // UI
 //= require ui/homepage_instagram.js
--- source/javascripts/ui/homepage_instagram.js
+++ source/javascripts/ui/homepage_instagram.js
@@ -6,20 +6,4 @@
 
 $(document).ready(function() {
 
-  //Initialize Instafeed.js on load
-  var feed = new Instafeed({
-    get: 'tagged',
-    tagName: 'video',
-    clientId: '20c5389504fc402d983e5685b859d6ba',
-    template: '<a class="animation" href="{{link}}"><img src="{{image}}" /></a><div>{{model.type}}</div>'
-  });
-  feed.run();
-
-  // Set a refresh of Instafeed every minute
-  var instafeedRefreshTimer = setInterval(RefreshTimer, 60000);
-  function RefreshTimer() {
-    feed.run();
-    scrollTop: $("#instafeed").offset().top
-  }
-
 });
