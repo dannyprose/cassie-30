328786ec3b3f0247cf040e8443d40c91a5698dbc
Danny Palmer <dannyp@bnj.com>
2014-03-16 17:35:28 -0700

    basic video.js setup

--- source/javascripts/ui/homepage_instagram.js
+++ source/javascripts/ui/homepage_instagram.js
@@ -4,7 +4,7 @@
 // Pull in all photos tagged with a certain hashtag
 ///////////////////////////////////////////////////
 
-var instagramTemplate = '<a href="#" class="instagram_photo" data-instagram_type={{model.type}}><img src="{{image}}" /></a><div class="instagram_photo_meta"><div class="instagram_photo_meta_caption">{{caption}}</div><div class="instagram_photo_meta_social"><div class="instagram_photo_user"><a href="http://www.instagram.com/{{model.user.username}}/"><img class="profile_picture" src="{{model.user.profile_picture}}"/>{{model.user.username}}</a></div><div class="instagram_photo_comments"><a href="{{link}}">{{comments}}</a></div><div class="instagram_photo_likes"><a href="{{link}}">{{likes}}</a></div></div></div>'
+var instagramTemplate = '<a class="instagram_photo" data-instagram-type={{model.type}} data-insagram-id={{id}} data-instagram-video-src="{{model.videos.standard_resolution.url}}" data-instagram-video-thumbnail="{{model.images.standard_resolution.url}}"><img class="instagram_photo_media" src="{{image}}" /></a><div class="instagram_photo_meta"><div class="instagram_photo_meta_caption">{{caption}}</div><div class="instagram_photo_meta_social"><div class="instagram_photo_user"><a href="http://www.instagram.com/{{model.user.username}}/"><img class="profile_picture" src="{{model.user.profile_picture}}"/>{{model.user.username}}</a></div><div class="instagram_photo_comments"><a href="{{link}}">{{comments}}</a></div><div class="instagram_photo_likes"><a href="{{link}}">{{likes}}</a></div></div></div>'
 
 $(document).ready(function() {
 
@@ -15,6 +15,24 @@ $(document).ready(function() {
     clientId: '20c5389504fc402d983e5685b859d6ba',
     template: instagramTemplate,
     resolution: 'standard_resolution',
+    after: function(){
+
+      //alert("Done!");
+
+      // Setup Videos
+      $('.instagram_photo[data-instagram-type="video"]').each(function(){
+        var videoId = $(this).attr("data-insagram-id");
+        var videoSource = $(this).attr("data-instagram-video-src");
+        var videoPoster = $(this).attr("data-instagram-video-thumbnail");
+        console.log(videoId);
+
+        var videoPlayer = '<video id="video_' + videoId + '" class="video-js vjs-default-skin" controls preload="auto" poster="' + videoPoster + '"data-setup="{"example_option":true}"><source src="' + videoSource + '" type="video/mp4" /></video>';
+
+        $(this).html(videoPlayer);
+
+      });
+
+    }
   });
   feed.run();
 
